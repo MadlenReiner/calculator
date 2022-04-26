ffd7e4e470d63a8d673cbb66154fab6207ae6d8c
Madlen Maria Reiner <madlen.reiner@univie.ac.at>
2022-04-26 16:43:26 +0200

    Added substract function to std_operations and used it in main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from std_operations import add
+from std_operations import substract
 
 a=-1
 b=3.14
 
 c=add(a,b)
+d=substract(a,b)
 
 print("{}+{}={}".format(a,b,c))
+print("{}-{}={}".format(a,b,d))

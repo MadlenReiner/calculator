94c3cdc0e2f8994b53cb1167e804e21becf5cd2a
Madlen Maria Reiner <madlen.reiner@univie.ac.at>
2022-05-03 12:19:52 +0200

    added multiply to std_operations, created linalg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from std_operations import add
-from std_operations import substract
+import numpy as np
+
+from std_operations import add, substract
+from linalg import ScalProd
 
 a=-1
 b=3.14
@@ -9,3 +11,10 @@ d=substract(a,b)
 
 print("{}+{}={}".format(a,b,c))
 print("{}-{}={}".format(a,b,d))
+
+u = np.array([1, 0.5, -3.14])
+v = np.array([-4, 0, 6.73])
+
+udotv = ScalProd(u,v)
+
+print("{} dot {} = {}".format(u,v,udotv))
